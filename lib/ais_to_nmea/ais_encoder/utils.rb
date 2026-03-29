e9f2f3a2fa2d41c4eeaae1577105539dc775fdd0
armandfardeau <fardeauarmand@gmail.com>
2026-03-29 15:34:39 +0200

    Remove utils

--- lib/ais_to_nmea/ais_encoder/utils.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module AisToNmea
-  module AisEncoder
-    # Utility helpers used across AIS field extraction and packing.
-    module Utils
-    end
-  end
-end
--- spec/ais_to_nmea/ais_encoder/utils_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-RSpec.describe AisToNmea::AisEncoder::Utils do
-  it 'is defined' do
-    expect(described_class).not_to be_nil
-  end
-end
